668868e0bfbdb5a6d78a6f9e2af6fd21c1de9c64
Maxence Roux <maxence.b.roux@gmail.com>
2025-06-11 15:57:40 +0200

    Add lulu jose lea

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749645168|1967489090';
+const CACHE_VERSION = '1749650243|7042389136';
 /** @type {string} */
 const CACHE_PREFIX = 'pacs_peche-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
---
+++ scenes/characters/jose/jose.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+var balloon_scene = preload("res://dialogue/game_dialogue_balloon.tscn")
+var in_range: bool
+@onready var interactable_component: InteractableComponent = $InteractableComponent
+@onready var interactable_label_component: Control = $InteractableLabelComponent
+
+
+func _ready() -> void:
+	interactable_component.interactable_activated.connect(on_interactable_activated)
+	interactable_component.interactable_deactivated.connect(on_interactable_deactivated)
+	interactable_label_component.hide()
+
+func on_interactable_activated() -> void: 
+	interactable_label_component.show()
+	in_range = true
+	
+func on_interactable_deactivated() -> void: 
+	interactable_label_component.hide()
+	in_range = false
+
+func _unhandled_input(event: InputEvent) -> void: 
+	if in_range:
+		if event.is_action_pressed("show_dialogue"):
+			var balloon: BaseGameDialogueBalloon = balloon_scene.instantiate()
+			
+			get_tree().current_scene.add_child(balloon)
+			balloon.queue_emote("jose")  # ← only works if you followed earlier setup
+			balloon.start(load("res://dialogue/conversations/jose.dialogue"), "start")

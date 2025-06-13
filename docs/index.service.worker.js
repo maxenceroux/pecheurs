79c223f7a56c7e7d0eb838f1be565ff751bf72ec
Maxence Roux <maxence.b.roux@gmail.com>
2025-06-13 14:58:35 +0200

    Add menu

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749650577|7376645112';
+const CACHE_VERSION = '1749819493|13150284179';
 /** @type {string} */
 const CACHE_PREFIX = 'pacs_peche-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
--- scenes/test/menu.gd
+++ scenes/test/menu.gd
@@ -1,6 +1,33 @@
 extends Control
 
-
-func _on_start_pressed() -> void:
-	print("Start pressed	")
+func _on_ludo_pressed() -> void:
+	print("Start ludo")
 	get_tree().change_scene_to_file("res://scenes/test/test_scene_house.tscn")
+
+
+
+
+func _on_laura_pressed() -> void:
+	print("Start laura")
+	var house_scene = load("res://scenes/test/test_scene_house.tscn").instantiate()
+	var player_2_scene = load("res://scenes/characters/player/player_2.tscn").instantiate()
+
+	# Find and replace the Player node
+	var old_player = house_scene.get_node_or_null("GameTileMap/Player")
+	if old_player:
+		print("hes")
+		var parent = old_player.get_parent()
+		var position = old_player.global_position
+		var scale = old_player.scale  # ✅ capture the scale
+		old_player.queue_free()
+
+		player_2_scene.global_position = position
+		player_2_scene.scale = Vector2(0.03, 0.03)  # ✅ set scale manually
+
+
+		player_2_scene.name = "Player"
+		parent.add_child(player_2_scene)
+
+	get_tree().root.add_child(house_scene)
+	get_tree().current_scene.queue_free()
+	get_tree().current_scene = house_scene
